cfd1eb9dd0d487d0952d58e0fc3c8d21e276f3d7
Tyberius Prime <tyberius_prime@coonabibba.de>
2024-10-18 20:15:52 +0200

    zstd compression

--- src/main.rs
+++ src/main.rs
@@ -166,18 +166,21 @@ impl std::fmt::Debug for Molecule {
 enum Reader {
     Raw(BufReader<std::fs::File>),
     Gzip(BufReader<flate2::read::GzDecoder<BufReader<std::fs::File>>>),
+    Zstd(BufReader<zstd::stream::Decoder<'static, BufReader<std::fs::File>>>),
 }
 
-enum Writer {
+enum Writer<'a> {
     Raw(BufWriter<std::fs::File>),
     Gzip(GzEncoder<BufWriter<std::fs::File>>),
+    Zstd(zstd::stream::AutoFinishEncoder<'a, BufWriter<std::fs::File>>),
 }
 
-impl Write for Writer {
+impl<'a> Write for Writer<'a> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         match self {
             Writer::Raw(inner) => inner.write(buf),
             Writer::Gzip(inner) => inner.write(buf),
+            Writer::Zstd(inner) => inner.write(buf),
         }
     }
 
@@ -185,6 +188,7 @@ impl Write for Writer {
         match self {
             Writer::Raw(inner) => inner.flush(),
             Writer::Gzip(inner) => inner.flush(),
+            Writer::Zstd(inner) => inner.flush(),
         }
     }
 }
@@ -224,6 +228,10 @@ impl Reader {
         line4.pop();
 
         if line2.len() != line4.len() {
+            dbg!(&std::str::from_utf8(&line1));
+            dbg!(&std::str::from_utf8(&line2));
+            dbg!(&std::str::from_utf8(&line3));
+            dbg!(&std::str::from_utf8(&line4));
             panic!("Truncated fastq file")
         }
 
@@ -237,6 +245,7 @@ impl Reader {
         match self {
             Reader::Raw(reader) => Reader::_next_read(reader),
             Reader::Gzip(reader) => Reader::_next_read(reader),
+            Reader::Zstd(reader) => Reader::_next_read(reader),
         }
     }
 }
@@ -248,7 +257,7 @@ struct InputSet {
     index2: Option<Reader>,
 }
 
-impl Iterator for &mut InputSet {
+impl<'a> Iterator for &mut InputSet {
     type Item = Molecule;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -307,43 +316,34 @@ fn open_file(path: &str) -> Result<Reader> {
     if magic[0] == 0x1f && magic[1] == 0x8b {
         let gz = BufReader::new(flate2::read::GzDecoder::new(bufreader));
         Ok(Reader::Gzip(gz))
+    } else if magic[0] == 0x28 && magic[1] == 0xb5 && magic[2] == 0x2f && magic[3] == 0xfd {
+        let zstd = zstd::stream::Decoder::with_buffer(bufreader)?;
+        Ok(Reader::Zstd(BufReader::new(zstd)))
     } else {
         let reader = Reader::Raw(bufreader);
         Ok(reader)
     }
 }
 
-fn open_input_files(parsed_config: &Config) -> Result<InputFiles> {
+fn open_input_files<'a>(input_config: crate::config::ConfigInput) -> Result<InputFiles> {
     let mut sets = Vec::new();
-    for (ii, read1_filename) in (&parsed_config.input.read1).into_iter().enumerate() {
+    for (ii, read1_filename) in (&input_config.read1).into_iter().enumerate() {
         // we can assume all the others are either of the same length, or None
         let read1 = open_file(&read1_filename)?;
-        let read2 = parsed_config
-            .input
-            .read2
-            .as_ref()
-            .map(|x| open_file(&x[ii]));
+        let read2 = input_config.read2.as_ref().map(|x| open_file(&x[ii]));
         //bail if it's an Error
         let read2 = match read2 {
             Some(Ok(x)) => Some(x),
             Some(Err(e)) => return Err(e),
             None => None,
         };
-        let index1 = parsed_config
-            .input
-            .index1
-            .as_ref()
-            .map(|x| open_file(&x[ii]));
+        let index1 = input_config.index1.as_ref().map(|x| open_file(&x[ii]));
         let index1 = match index1 {
             Some(Ok(x)) => Some(x),
             Some(Err(e)) => return Err(e),
             None => None,
         };
-        let index2 = parsed_config
-            .input
-            .index2
-            .as_ref()
-            .map(|x| open_file(&x[ii]));
+        let index2 = input_config.index2.as_ref().map(|x| open_file(&x[ii]));
         let index2 = match index2 {
             Some(Ok(x)) => Some(x),
             Some(Err(e)) => return Err(e),
@@ -361,34 +361,40 @@ fn open_input_files(parsed_config: &Config) -> Result<InputFiles> {
 }
 
 #[derive(Default)]
-struct OutputFiles {
-    read1: Option<Writer>,
-    read2: Option<Writer>,
-    index1: Option<Writer>,
-    index2: Option<Writer>,
-    reports: Vec<Writer>,
+struct OutputFiles<'a> {
+    read1: Option<Writer<'a>>,
+    read2: Option<Writer<'a>>,
+    index1: Option<Writer<'a>>,
+    index2: Option<Writer<'a>>,
+    reports: Vec<Writer<'a>>,
     inspects: Vec<(
-        Option<Writer>,
-        Option<Writer>,
-        Option<Writer>,
-        Option<Writer>,
+        Option<Writer<'a>>,
+        Option<Writer<'a>>,
+        Option<Writer<'a>>,
+        Option<Writer<'a>>,
     )>,
 }
 
-fn open_raw_output_file(path: &str) -> Result<Writer> {
+fn open_raw_output_file<'a>(path: &str) -> Result<Writer<'a>> {
     let fh = std::fs::File::create(path).context("Could not open file.")?;
     let bufwriter = BufWriter::new(fh);
     Ok(Writer::Raw(bufwriter))
 }
 
-fn open_gzip_output_file(path: &str, level: flate2::Compression) -> Result<Writer> {
+fn open_gzip_output_file<'a>(path: &str, level: flate2::Compression) -> Result<Writer<'a>> {
     let fh = std::fs::File::create(path).context("Could not open file.")?;
     let buf_writer = BufWriter::new(fh);
     let gz = GzEncoder::new(buf_writer, level);
     Ok(Writer::Gzip(gz))
 }
-
-fn open_output_files(parsed_config: &Config) -> Result<OutputFiles> {
+fn open_zstd_output_file<'a>(path: &str, level: i32) -> Result<Writer<'a>> {
+    let fh = std::fs::File::create(path).context("Could not open file.")?;
+    let buf_writer = BufWriter::new(fh);
+    let encoder = zstd::stream::Encoder::new(buf_writer, level)?;
+    let encoder = encoder.auto_finish();
+    Ok(Writer::Zstd(encoder))
+}
+fn open_output_files<'a>(parsed_config: &Config) -> Result<OutputFiles<'a>> {
     Ok(match &parsed_config.output {
         Some(output_config) => {
             let suffix =
@@ -401,6 +407,7 @@ fn open_output_files(parsed_config: &Config) -> Result<OutputFiles> {
                         FileFormat::Zstd => ".fq.zst",
                     });
             let (read1, read2, index1, index2) = match output_config.format {
+                //todo: refactor
                 FileFormat::Raw => {
                     let read1 = Some(open_raw_output_file(&format!(
                         "{}_1{}",
@@ -458,7 +465,35 @@ fn open_output_files(parsed_config: &Config) -> Result<OutputFiles> {
 
                     (read1, read2, index1, index2)
                 }
-                _ => todo!(),
+                FileFormat::Zstd => {
+                    let read1 = Some(open_zstd_output_file(
+                        &format!("{}_1{}", output_config.prefix, suffix),
+                        5,
+                    )?);
+                    let read2 = match parsed_config.input.read2 {
+                        Some(_) => Some(open_zstd_output_file(
+                            &format!("{}_2{}", output_config.prefix, suffix),
+                            5,
+                        )?),
+                        None => None,
+                    };
+                    let (index1, index2) = if output_config.keep_index {
+                        (
+                            Some(open_zstd_output_file(
+                                &format!("{}_i1{}", output_config.prefix, suffix),
+                                5,
+                            )?),
+                            Some(open_zstd_output_file(
+                                &format!("{}_i2{}", output_config.prefix, suffix),
+                                5,
+                            )?),
+                        )
+                    } else {
+                        (None, None)
+                    };
+
+                    (read1, read2, index1, index2)
+                }
             };
             let reports = Vec::new();
             let inspects = Vec::new();
@@ -474,34 +509,6 @@ fn open_output_files(parsed_config: &Config) -> Result<OutputFiles> {
         }
         None => OutputFiles::default(),
     })
-}
-fn read_read(reader: &mut Reader) -> FastQRead {
-    match reader {
-        Reader::Raw(reader) => {
-            //todo: these ain't utf8.
-            //and I need to cut off teh @ from the name
-            let mut line1 = String::new();
-            let mut line2 = String::new();
-            let mut line3 = String::new();
-            let mut line4 = String::new();
-            reader
-                .read_line(&mut line1)
-                .expect("Could not read line 1.");
-            reader
-                .read_line(&mut line2)
-                .expect("Could not read line 2.");
-            reader.read_line(&mut line3).expect("Could not read line.");
-            reader
-                .read_line(&mut line4)
-                .expect("Could not read line 4.");
-            let name = line1.into_bytes();
-            let seq = line2.into_bytes();
-            let qual = line4.into_bytes();
-
-            FastQRead { name, seq, qual }
-        }
-        Reader::Gzip(_) => todo!(),
-    }
 }
 
 struct Work {
@@ -543,99 +550,102 @@ fn main() -> Result<()> {
     let raw_config = ex::fs::read_to_string(toml_file).context("Could not read toml file.")?;
     let parsed = toml::from_str::<Config>(&raw_config).context("Could not parse toml file.")?;
     check_config(&parsed)?;
-    let mut input_files = open_input_files(&parsed)?;
-    let mut output_files = open_output_files(&parsed)?;
+    {
+        let mut input_files = open_input_files(parsed.input.clone())?;
+        let mut output_files = open_output_files(&parsed)?;
 
-    use crossbeam::channel::bounded;
-    let channel_size = 50;
+        use crossbeam::channel::bounded;
+        let channel_size = 50;
 
-    let stages = split_transforms_into_stages(&parsed.transform);
-    //dbg!(&stages);
+        let stages = split_transforms_into_stages(&parsed.transform);
+        //dbg!(&stages);
 
-    let channels: Vec<_> = (0..stages.len() + 1)
-        .into_iter()
-        .map(|_| {
-            let (tx, rx) = bounded(channel_size);
-            (tx, rx)
-        })
-        .collect();
+        let channels: Vec<_> = (0..stages.len() + 1)
+            .into_iter()
+            .map(|_| {
+                let (tx, rx) = bounded(channel_size);
+                (tx, rx)
+            })
+            .collect();
 
-    let block_size = parsed.options.block_size;
-    let input_channel = channels[0].0.clone();
-    let reader = thread::spawn(move || {
-        let mut block_no = 1;
-        for block in &(&mut input_files).chunks(block_size) {
-            let block: Vec<_> = block.collect();
-            input_channel.send(Work { block_no, block }).unwrap();
-            block_no += 1;
-        }
-    });
+        let block_size = parsed.options.block_size;
+        let input_channel = channels[0].0.clone();
+        let reader = thread::spawn(move || {
+            let mut block_no = 1;
+            for block in &(&mut input_files).chunks(block_size) {
+                let block: Vec<_> = block.collect();
+                input_channel.send(Work { block_no, block }).unwrap();
+                block_no += 1;
+            }
+        });
 
-    let thread_count = parsed.options.thread_count;
-    for (stage_no, (stage, needs_serial)) in stages.into_iter().enumerate() {
-        let local_thread_count = if needs_serial { 1 } else { thread_count };
-        for thread_ii in 0..local_thread_count {
-            let mut stage = stage.clone();
-            let input_rx2 = channels[stage_no].1.clone();
-            let output_tx2 = channels[stage_no + 1].0.clone();
-            let processor = thread::spawn(move || loop {
-                match input_rx2.recv() {
-                    Ok(block) => {
-                        let mut out_block = block.block;
-                        for stage in stage.iter_mut() {
-                            out_block = stage.transform(out_block);
+        let thread_count = parsed.options.thread_count;
+        for (stage_no, (stage, needs_serial)) in stages.into_iter().enumerate() {
+            let local_thread_count = if needs_serial { 1 } else { thread_count };
+            for thread_ii in 0..local_thread_count {
+                let mut stage = stage.clone();
+                let input_rx2 = channels[stage_no].1.clone();
+                let output_tx2 = channels[stage_no + 1].0.clone();
+                let processor = thread::spawn(move || loop {
+                    match input_rx2.recv() {
+                        Ok(block) => {
+                            let mut out_block = block.block;
+                            for stage in stage.iter_mut() {
+                                out_block = stage.transform(out_block);
+                            }
+                            output_tx2
+                                .send(Work {
+                                    block_no: block.block_no,
+                                    block: out_block,
+                                })
+                                .unwrap();
                         }
-                        output_tx2
-                            .send(Work {
-                                block_no: block.block_no,
-                                block: out_block,
-                            })
-                            .unwrap();
+                        Err(e) => {
+                            return;
+                        }
                     }
-                    Err(e) => {
-                        return;
-                    }
-                }
-            });
+                });
+            }
         }
-    }
 
-    let output_channel = (channels[channels.len() - 1]).1.clone();
-    let output = thread::spawn(move || {
-        let mut last_block_outputted = 0;
-        let mut buffer = Vec::new();
-        loop {
-            match output_channel.recv() {
-                Ok(block) => {
-                    buffer.push(block);
-                    loop {
-                        let mut send = None;
-                        for (ii, block) in buffer.iter().enumerate() {
-                            if block.block_no - 1 == last_block_outputted {
-                                last_block_outputted += 1;
-                                send = Some(ii);
+        let output_channel = (channels[channels.len() - 1]).1.clone();
+        let output = thread::spawn(move || {
+            let mut last_block_outputted = 0;
+            let mut buffer = Vec::new();
+            loop {
+                match output_channel.recv() {
+                    Ok(block) => {
+                        buffer.push(block);
+                        loop {
+                            let mut send = None;
+                            for (ii, block) in buffer.iter().enumerate() {
+                                if block.block_no - 1 == last_block_outputted {
+                                    last_block_outputted += 1;
+                                    send = Some(ii);
+                                    break;
+                                }
+                            }
+                            if let Some(send_idx) = send {
+                                let to_output = buffer.remove(send_idx);
+                                output_block(to_output.block, &mut output_files);
+                            } else {
                                 break;
                             }
                         }
-                        if let Some(send_idx) = send {
-                            let to_output = buffer.remove(send_idx);
-                            output_block(to_output.block, &mut output_files);
-                        } else {
-                            break;
-                        }
+                    }
+                    Err(e) => {
+                        break;
                     }
                 }
-                Err(e) => {
-                    break;
-                }
             }
-        }
-    });
-    drop(channels); //we must not hold a reference here across the join at the end
-    let _ = output.join();
-    //ok all this needs is a buffer that makes sure we reorder correctly at the end.
-    //and then something block based, not single reads to pass between the threads.
-
+        });
+        drop(channels); //we must not hold a reference here across the join at the end
+        let _ = output.join();
+        let _ = reader.join();
+        //ok all this needs is a buffer that makes sure we reorder correctly at the end.
+        //and then something block based, not single reads to pass between the threads.
+        drop(parsed);
+    }
     Ok(())
 }
 
