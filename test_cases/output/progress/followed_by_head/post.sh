c4fcb0bc92018f632f4341cce57e43dae2ae24a0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2026-01-14 15:09:56 +0100

    make progress followed by head test more robust

--- test_cases/output/progress/followed_by_head/post.sh
+++ test_cases/output/progress/followed_by_head/post.sh
@@ -2,10 +2,16 @@
 
 data=$(<output_run.progress)
 
-pos0=$(echo "$data" | grep -n "Processed Total: 0" | cut -d: -f1)
+if grep -q "337_903 molecules" output_run.progress; then
+    echo "Error: '337_903 molecules' was found in file"
+    cat output_run.progress
+    exit 1
+fi
 
-if ! grep -q "10_000 molecules" output_run.progress; then
-    echo "Error: '10_000 molecules' not found in file"
+ #verify that 10_000, 20_000 or similar low multple of 10k was found
+ if ! grep -q "to process 10_000\|20_000\|30_000\|40_000\|50_000 molecules" output_run.progress; then
+    echo "Error: Expected 'Processed Total: x0_000' not found"
+    cat output_run.progress
     exit 1
 fi
 
