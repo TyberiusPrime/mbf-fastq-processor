d0ed790d23efe55fae9569b550b0521f1005efdf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2026-01-21 16:48:51 +0100

    update rust & use rust-matching rust-analyzer

--- flake.nix
+++ flake.nix
@@ -22,9 +22,9 @@
         #pkgs = nixpkgs.legacyPackages."${system}";
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        rust = pkgs.rust-bin.stable."1.90.0".default.override {
+        rust = pkgs.rust-bin.stable."1.92.0".default.override {
           targets = [ "x86_64-unknown-linux-musl" ];
-          extensions = [ "llvm-tools-preview" ];
+          extensions = [ "llvm-tools-preview"  "rust-analyzer"];
         };
 
         # Override the version used in naersk
@@ -276,7 +276,7 @@
             pkgs.rapidgzip
             pkgs.which
             pkgs.ripgrep
-            pkgs.rust-analyzer
+            #rust.rust-analyzer
             pkgs.shellcheck
             rust
           ];
